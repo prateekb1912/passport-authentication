376a60214414396e3f0f90aeb9a435571edd3ddb
Prateek Bhardwaj <prateekb1912@gmail.com>
2023-05-24 23:36:22 +0530

    Complete view layouts for all routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/login', (req, res) => res.json({'message': 'Success login'}))    
-router.get('/register', (req, res) => res.json({'message': 'Success register'}))    
+router.get('/login', (req, res) => res.render('login'))    
+router.get('/register', (req, res) => res.render('register'))    
 
 module.exports = router;
