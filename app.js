27e0a772bf21ee43bfcd94e8a9304482dd8e53fb
Prateek Bhardwaj <prateekb1912@gmail.com>
2023-05-24 23:41:42 +0530

    Init mongoose and connect to atlas db

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// DB configuration
+const db = require('./config/keys').MongoURI;
+
+mongoose.connect(db, {useNewURLParser: true})
+    .then(() => console.log('Database connection success'))
+    .catch(err => console.log(err));
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://root:Sfl0XGEBHuwsqkul@auth-cluster.pddbeav.mongodb.net/?retryWrites=true&w=majority'
+}
