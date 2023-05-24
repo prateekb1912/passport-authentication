74f63e6518bf7a3c3571c515f7004386e1018598
Prateek Bhardwaj <prateekb1912@gmail.com>
2023-05-24 23:10:31 +0530

    Init index and user routes for dashboard and login

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
+
 app.listen(PORT, console.log(`Server started on http://localhost:${PORT}`))
