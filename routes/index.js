4de7c0012b81ab9b381b13a6807e84fa6df15a4a
Prateek Bhardwaj <prateekb1912@gmail.com>
2023-05-24 23:21:30 +0530

    Stylize base layout template for rendering

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.json({'message': 'Success'}))
+router.get('/', (req, res) => res.render('welcome'))
 
 module.exports = router;
